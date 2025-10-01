de1acf76592f5273d9cdae2b6d209f17db60bf47
Huadong PENG <109780922+0bangbangda@users.noreply.github.com>
2025-10-01 20:48:54 +0800

    Update cnn.c

--- cnn.c
+++ cnn.c
@@ -14,13 +14,13 @@ Conv1_28x28x1_5x5x20_1_0(input, conv1_kernel, conv1_bias, conv1_output){
     for(int k=0;k<CONV1_NBOUTPUT;k++){//depth of output 
         for(int y=0;y<CONV1_HEIGHT;y++){//output y axis
             for(int x=0;x<CONV1_WIDTH;x++){//output x axis
-                conv1_output[k][y][x]=conv1_bias[k];//bias
+                output[k][y][x]=bias[k];//bias
                 for(int j=0;j<CONV1_DIM;j++){//kernel y axis
                     for(int i=0;i<CONV1_DIM;i++){//kernel x axis
-                        conv1_output[k][y][x]+=input[0][y+j][x+i]*conv1_kernel[k][0][j][i];
+                        output[k][y][x]+=input[0][y+j][x+i]*kernel[k][0][j][i];
                     }
                 }
-                if(conv1_output[k][y][x]<0) conv1_output[k][y][x]=0; //ReLU
+                if(output[k][y][x]<0) output[k][y][x]=0; //ReLU
             }
         }
     }
@@ -39,17 +39,19 @@ int max(int filtre[2][2]){
     max2=(filtre[1][0]>filtre[1][1])?filtre[1][0]:filtre[1][1];
     return (max1>max2)?max1:max2;
 }
-Pool1_24x24x20_2x2x20_2_0(conv1_output, pool1_output){
+void Pool1_24x24x20_2x2x20_2_0(	float 	input[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_WIDTH], 	    // IN
+    float 	output[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH]);		// OUT
+{
     int filtre[2][2];
     for(int i=0;i<POOL1_NBOUTPUT;i+=1){//depth of pool1
         for(int y=0;y<POOL1_HEIGHT;y++){//pool1_outpu y axis
             for(int x=0;x<POOL1_WIDTH;x++){//pool1_outpu x axis
-                for(int k=0;k<22;k+=2){//conv1_output matrice
-                    filtre[0][0]=conv1_output[i][k][k];
-                    filtre[0][1]=conv1_output[i][k][k+1];
-                    filtre[1][0]=conv1_output[i][k+1][k];
-                    filtre[1][1]=conv1_output[i][k+1][k+1];
-                    pool1_output[y][x]=max(filtre);
+                for(int k=0;k<CONV1_HEIGHT-2;k+=2){//conv1_output matrice
+                    filtre[0][0]=input[i][k][k];
+                    filtre[0][1]=input[i][k][k+1];
+                    filtre[1][0]=input[i][k+1][k];
+                    filtre[1][1]=input[i][k+1][k+1];
+                    output[y][x]=max(filtre);
                 }
             }
         }
@@ -57,27 +59,57 @@ Pool1_24x24x20_2x2x20_2_0(conv1_output, pool1_output){
 }
 /*
 parametres
-pool1_output:12x12x20
-conv2_kernel:5x5x40
-conv2_bias:40
-conv2_output:8x8x40
-
+input:20x12x12
+kernel:40x20x5x5
+bias:40
+output:40x8x8
 */
-Conv2_12x12x20_5x5x40_1_0(pool1_output, conv2_kernel, conv2_bias, conv2_output){
-    for(int k=0;k<CONV2_NBOUTPUT;k++){//depth of output 
+void Conv2_12x12x20_5x5x40_1_0(	float input[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH], 	            // IN
+    float kernel[CONV2_NBOUTPUT][POOL1_NBOUTPUT][CONV2_DIM][CONV2_DIM], 	// IN
+    float bias[CONV2_NBOUTPUT], 						                    // IN
+    float output[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WIDTH]); 		        // OUT
+{    
+    for(int k=0;k<CONV2_NBOUTPUT;k++){//depth of output
+        for(int m=0;m<POOL1_NBOUTPUT;m++){ 
         for(int y=0;y<CONV2_HEIGHT;y++){//output y axis
             for(int x=0;x<CONV2_WIDTH;x++){//output x axis
-                conv2_output[k][y][x]=conv2_bias[k];//bias
+                output[k][y][x]=bias[k];//bias
                 for(int j=0;j<CONV2_DIM;j++){//kernel y axis
                     for(int i=0;i<CONV2_DIM;i++){//kernel x axis
-                        conv2_output[k][y][x]+=pool1_output[0][y+j][x+i]*conv1_kernel[k][0][j][i];
+                        output[k][y][x]+=input[m][y+j][x+i]*kernel[k][m][j][i];
                     }
                 }
-                if(conv1_output[k][y][x]<0) conv1_output[k][y][x]=0; //ReLU
+            }
+        }
+    }
+    if(output[k][y][x]<0) output[k][y][x]=0; //ReLU
+    }
+}
+/*
+parametres:
+input:40x8x8
+output:40x4x4
+
+*/
+void Pool2_8x8x40_2x2x40_2_0(	float 	input[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WIDTH], 	    // IN
+    float 	output[POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH]);		// OUT
+{
+    int filtre[2][2];
+    for(int i=0;i<POOL2_NBOUTPUT;i+=1){//depth of pool2
+        for(int y=0;y<POOL2_HEIGHT;y++){//pool1_outpu y axis
+            for(int x=0;x<POOL2_WIDTH;x++){//pool1_outpu x axis
+                for(int k=0;k<CONV2_HEIGHT-2;k+=2){//conv1_output matrice
+                    filtre[0][0]=input[i][k][k];
+                    filtre[0][1]=input[i][k][k+1];
+                    filtre[1][0]=input[i][k+1][k];
+                    filtre[1][1]=input[i][k+1][k+1];
+                    output[y][x]=max(filtre);
+                }
             }
         }
     }
 }
+
 /*
 parametres
 input:40x4x4
@@ -91,5 +123,36 @@ void Fc1_40_400(	float 	input[POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH], 			   
     float 	bias[FC1_NBOUTPUT],							                        // IN
     float 	output[FC1_NBOUTPUT]); 							                    // OUT
 {
+    for(int k=0;k<FC1_NBOUTPUT;k++){
+        output[k]=bias[k];
+        for(int i=0;i<POOL2_NBOUTPUT;i++){
+            for(int y=0;y<POOL2_HEIGH;y++){
+                for(int x=0;x<POOL2_WIDTH;x++){
+                    output[k]+=kernel[k][i][y][x]*input[i][y][x];
+                }
 
+            }
+        }
+        if(output[k]<0) output[k]=0;
+    }
+
+}
+/*
+parametres
+input:400
+kernel:10x400
+bias:10
+output:10
+*/
+void Fc2_400_10(	float 	input[FC1_NBOUTPUT], 			        // IN
+    float 	kernel[FC2_NBOUTPUT][FC1_NBOUTPUT],	    // IN
+    float 	bias[FC2_NBOUTPUT],			            // IN
+    float 	output[FC2_NBOUTPUT]); 			        // OUT
+{
+    for(int k=0;k<FC2_NBOUTPUT;k++){
+        output[k]=bias[k];
+        for(int i=0;i<FC1_NBOUTPUT;i++){
+            output[k]+=input[i]*kernel[k][i];
+        }
+        if(output[k]<0) output[k]=0;
 }
